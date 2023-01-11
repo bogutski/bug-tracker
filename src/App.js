8109fc4bb9129d92b92b869ac2f4cc79d83a8085
mariaSOS <ms2145051478@gmail.com>
2023-01-10 20:46:56 -0600

    deleted app data for react app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
-
+import "./App.css";
 function App() {
+
   return (
     <div className="App">
       <h1>Hello world!</h1>
---
+++ src/dbConnection.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDciBx0LD55KYfGve75RTXy6rJKxUOQbVk",
+  authDomain: "bugtracker-e33ca.firebaseapp.com",
+  projectId: "bugtracker-e33ca",
+  storageBucket: "bugtracker-e33ca.appspot.com",
+  messagingSenderId: "26112839778",
+  appId: "1:26112839778:web:86f0b4a9f90b8da059c13b",
+  measurementId: "G-M5QE6SVQQD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+console.log(db)
+export default db;
