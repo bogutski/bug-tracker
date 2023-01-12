8b8e628cb338b73ed40bdcb66a704f3c62b0da22
mariaSOS <ms2145051478@gmail.com>
2023-01-12 17:55:13 -0600

    corrected login for reflection create board button

--- src/components/CreateBoard.js
+++ src/components/CreateBoard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CreateBoard = () => {
     return (
         <div>
-            
+            <button type="button" className="btn btn-link">Create Board</button>
         </div>
     );
 };
--- src/components/boards/BoardList.js
+++ src/components/boards/BoardList.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import BoardItem from "./BoardItem";
+import CreateBoard from "../CreateBoard";
 
 const BoardList = (props) => {
     const {boards, projectId} = props
     return (
       <div className="dropdown-menu">
           {!projectId ?
-            boards?.map(board => <BoardItem key={board.id} board={board}/>)
-            : boards?.filter(board => board.projectId === projectId).map(board => <BoardItem key={board.id} board={board}/>)}
+            boards?.map(board => <BoardItem key={board.id} board={board}/>) : boards?.filter(board => board.projectId === projectId).map(board =>
+              <BoardItem key={board.id} board={board}/>)}
+        {projectId && <div>
+          <div className="dropdown-divider"></div>
+          <CreateBoard/>
+        </div>
+        }
       </div>
-    );
+        );
 };
 
 export default BoardList;
