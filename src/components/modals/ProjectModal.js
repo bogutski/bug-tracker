473ac3fed44849979165b83355ff730c3237c94d
vikta <vik.tan@hotmail.com>
2023-01-13 14:17:19 -0600

    added Create and Update Project functionality

--- src/components/modals/ProjectModal.js
+++ src/components/modals/ProjectModal.js
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
+import {collection, addDoc, updateDoc, doc} from "firebase/firestore";
+import db from "../../dbConnection";
 
-const ProjectModal = () => {
+function ProjectModal(props) {
+
+    const {modal, toggle, title, projname, projectId} = props;
+    const [projectName, setProjectName] = useState(projname);
+    const createProject = (e) => {
+        e.preventDefault();
+        const refDoc = collection(db, 'Project');
+        addDoc(refDoc, {
+            projectName
+        }).then(res => res.id).catch(err => console.log(err));
+        setProjectName('');
+    }
+    const updateProject = (e) => {
+        e.preventDefault();
+        updateDoc(doc(db, 'Project', projectId), {projectName})
+            .then(r => console.log(r))
+            .catch(err => console.log(err));
+    }
+
+    function saveButtonHandler(e) {
+        if (title === 'Create Project') {
+            createProject(e);
+        } else {
+            updateProject(e);
+        }
+        toggle();
+    }
+
     return (
         <div>
-            
+
+            <Modal isOpen={modal} toggle={toggle} {...props}>
+                <ModalHeader toggle={toggle}>{title}</ModalHeader>
+                <ModalBody>
+                    <div className="input-group mb-3">
+                        <span className="input-group-text" id="basic-addon1">Project name:</span>
+                        <input
+                            type="text"
+                            className="form-control"
+                            aria-describedby="basic-addon1"
+                            value={projectName}
+                            onChange={e => setProjectName(e.target.value)}
+                        />
+                    </div>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" onClick={(e) => saveButtonHandler(e)}>
+                        Save
+                    </Button>{' '}
+                    <Button color="secondary" onClick={toggle}>
+                        Cancel
+                    </Button>
+                </ModalFooter>
+            </Modal>
         </div>
     );
-};
+}
 
 export default ProjectModal;
