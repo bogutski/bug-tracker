ceaf62552ccb1136046d08076b70cc06d61f4c33
kristinavan <chr1st1nko007@gmail.com>
2023-01-17 12:57:54 -0500

    add functionality for update button

--- src/components/modals/BoardModal.js
+++ src/components/modals/BoardModal.js
@@ -1,16 +1,43 @@
 import React, {useState} from 'react';
 import {Button, Input, InputGroup, InputGroupText, Modal, ModalBody, ModalFooter, ModalHeader} from "reactstrap";
+import {addDoc, collection, doc, updateDoc} from "firebase/firestore";
+import db from "../../dbConnection";
 
 const BoardModal = (props) => {
 
-    const {modal, toggle} = props;
-    const [boardName, setBoardName] = useState();
+    const {modal, toggle, title, currentBoard, boardId} = props;
+    const [boardName, setBoardName] = useState(currentBoard);
 
+    const createBoard = (e) => {
+        e.preventDefault();
+        const refBoardDoc = collection(db, 'Boards');
+        addDoc(refBoardDoc, {
+            boardName
+        })
+            .then(res => res.id)
+            .catch(err => console.log(err));
+        setBoardName('');
+    }
+    const updateBoard = (e) => {
+        e.preventDefault();
+        updateDoc(doc(db, 'Boards', boardId), {boardName})
+            .then(r => console.log(r))
+            .catch(err => console.log(err));
+    }
+
+    function okButtonHandler(e) {
+        if (title === 'Create Board') {
+            createBoard(e);
+        } else {
+            updateBoard(e);
+        }
+        toggle();
+    }
 
     return (
         <div>
             <Modal isOpen={modal} toggle={toggle} {...props}>
-                <ModalHeader toggle={toggle}>Create Board</ModalHeader>
+                <ModalHeader toggle={toggle}>{title}</ModalHeader>
                 <ModalBody>
                     <InputGroup>
                         <InputGroupText>
@@ -23,7 +50,7 @@ const BoardModal = (props) => {
                     </InputGroup>
                 </ModalBody>
                 <ModalFooter>
-                    <Button color="primary" onClick={toggle}>
+                    <Button color="primary" onClick={e => okButtonHandler(e)}>
                         Ok
                     </Button>{' '}
                     <Button color="secondary" onClick={toggle}>
