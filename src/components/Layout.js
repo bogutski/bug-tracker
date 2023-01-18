011ade7f6fe85fdc24b85946e62dddf7852eafa3
mariaSOS <ms2145051478@gmail.com>
2023-01-18 16:25:24 -0600

    some correction

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,13 +11,12 @@ const Layout = (props) => {
     return (
         <>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">🐞Bug-Tracker </a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-                    aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse d-flex justify-content-around" id="navbarNavDropdown">
+
+            <div className="collapse navbar-collapse d-flex justify-content-between" id="navbarNavDropdown">
                 <ul className="navbar-nav">
+                    <li>
+                    <a className="navbar-brand" href="#">🐞Bug-Tracker </a>
+                    </li>
                     <li className="nav-item active">
                         <a className="nav-link" href="/">Dashboard <span className="sr-only">(current)</span></a>
                     </li>
