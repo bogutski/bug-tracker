4b3cbc07a556be635a337df516720507b457a381
Kristina <72752511+Kristina-Van@users.noreply.github.com>
2023-01-17 23:22:25 -0500

    Create board (#12)
    * Created CreateBoard, BoardItem, BoardList, BoardModal
    
    * deleted list from boards button
    
    * deleted comments
    
    * deleted board button
    
    * deleted create board button
    
    * add functionality for update button
    
    * added logic of connection btw project & board
    
    * deleted consol.log
    
    Co-authored-by: mariaSOS <ms2145051478@gmail.com>

--- src/components/boards/Board.js
+++ src/components/boards/Board.js
@@ -2,9 +2,6 @@ import React, {useState} from 'react';
 import {useParams} from "react-router-dom";
 import BoardModal from "../modals/BoardModal";
 import BoardList from "./BoardList";
-import {doc, updateDoc} from "firebase/firestore";
-import db from "../../dbConnection";
-
 
 const Board = (props) => {
 
@@ -16,28 +13,21 @@ const Board = (props) => {
     const params = useParams();
     const boardId = params.boardId;
 
-    const currentBoard = boards.filter(board => board.id === params.boardId)[0]
-
-    const updateBoard = (e) => {
-        e.preventDefault();
-        updateDoc(doc(db, 'Board', boardId), {boardName})
-            .then(r => console.log(r))
-            .catch(err => console.log(err));
-    }
+    const currentBoard = boards?.find(board => board.id === params.boardId)?.boardName;
 
 
     return (
         <div>
+
             {modal && <BoardModal
                 modal={modal}
                 toggle={toggle}
                 title={"Update Board"}
                 boardId={boardId}
                 currentBoard={currentBoard}
-                updateBoard={updateBoard}
             />}
 
-            <h1>Board: {boards?.find(board => board.id === params.boardId)?.boardName}</h1>
+            <h1>Board: {currentBoard}</h1>
 
             <button
                 className="btn btn-warning"
@@ -47,7 +37,6 @@ const Board = (props) => {
             >
                 Update
             </button>
-
 
             <BoardList  boardId={boardId}/>
         </div>
