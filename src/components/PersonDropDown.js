ca2509c7c30fa0cf2d8b4006a268255514ea1f60
mariaSOS <ms2145051478@gmail.com>
2023-01-24 11:44:15 -0600

    home page + added ava for user

--- src/components/PersonDropDown.js
+++ src/components/PersonDropDown.js
@@ -9,6 +9,9 @@ import {useNavigate} from "react-router-dom";
 import {auth} from "../dbConnection";
 import {signOut} from 'firebase/auth';
 
+const style= {
+    width: '50px'
+}
 function PersonDropDown({direction, authUser, ...args}) {
     const [dropdownOpen, setDropdownOpen] = useState(false);
 
@@ -32,12 +35,8 @@ function PersonDropDown({direction, authUser, ...args}) {
         <>
             <Dropdown isOpen={dropdownOpen} toggle={toggle} direction="down">
                 <DropdownToggle caret color="light">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                         className="bi bi-person"
-                         viewBox="0 0 16 16">
-                        <path
-                            d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
-                    </svg>
+                    <img src="https://mdbcdn.b-cdn.net/img/new/avatars/1.webp" className="rounded-circle shadow-4"
+                         style={style} alt="Avatar"/>
                 </DropdownToggle>
                 <DropdownMenu {...args} end={true}>
                     {authUser.email && <>
