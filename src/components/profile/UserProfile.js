3215f04b8f861b3f5b14a79169101745f10a82c1
Kristina <72752511+Kristina-Van@users.noreply.github.com>
2023-01-24 22:01:21 -0500

    created UserProfile, EditUserProfile and UserProfileItem and link to homepage (#23)

---
+++ src/components/profile/UserProfile.js
@@ -0,0 +1,69 @@
+import React, {useState} from 'react';
+import {Alert, Button, Card, CardBody, CardSubtitle, CardText, CardTitle} from "reactstrap";
+import EditUserProfile from "./EditUserProfile";
+
+const UserProfile = () => {
+
+    const [modal, setModal] = useState(false);
+    const toggle = () => setModal(!modal);
+
+
+        return (
+
+            <div>
+                <Alert color="warning">
+                    Go To {' '}
+                    <a
+                        className="alert-link"
+                        href="http://localhost:3000/"
+                        rel="noreferrer"
+                        target="_blank"
+                    >
+                        Home Page
+                    </a>
+                </Alert>
+                <div className="col-4">
+                <Card
+                    style={{
+                        width: '25rem'
+
+                    }}
+                >
+                    <img
+                        alt="Sample"
+                        src="https://mdbcdn.b-cdn.net/img/new/avatars/1.webp"
+                    />
+                    <CardBody>
+                        <CardTitle tag="h5">
+                            First Name & Last Name
+                        </CardTitle>
+                        <CardSubtitle
+                            className="mb-2 text-muted"
+                            tag="h6"
+                        >
+                            📬 E-mail
+                        </CardSubtitle>
+                        <CardSubtitle
+                            className="mb-2 text-muted"
+                            tag="h6"
+                        >
+                            📞 Phone Number
+                        </CardSubtitle>
+                        <CardText>
+                            🏠 Address
+                        </CardText>
+                        <Button type="button" className="btn btn-outline-warning" onClick={toggle}>
+                            📝 Edit Profile
+                        </Button>
+                        {modal && <EditUserProfile
+                            modal={modal}
+                            toggle={toggle}
+                        />}
+                    </CardBody>
+                </Card>
+                </div>
+            </div>
+        );
+};
+
+export default UserProfile;
